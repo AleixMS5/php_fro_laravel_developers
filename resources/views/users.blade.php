d9e21e9da27a3309710927c9706c1349630d0bd6
alumne <amontero@iesebre.com>
2022-11-03 17:49:39 +0100

    final

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,5 +1,21 @@
 <x-layout>
-<div>
-users here
-</div>
+    <table class="table">
+        <tr>
+            <th>Id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>password</th>
+        </tr>
+        @foreach ($users as $user)
+
+            <tr>
+                <td> {{$user->id}}</td>
+                <td> {{$user->name}}</td>
+                <td> {{$user->email}}</td>
+                <td> {{$user->password}}</td>
+            </tr>
+        @endforeach
+
+
+    </table>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\UserController;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
@@ -16,35 +18,8 @@ use App\Http\Controllers\TaskController;
 
 Route::get('/', [TaskController::class,'index']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/tasks', function () {
-//    $task1 =Task::create([
-//        'title'=>'tasca1',
-//        'description'=>'tasca1',
-//        'completed'=>0
-//    ]);
-//
-//    $tasks = [
-//        $task1
-//    ];
-    return view('tasks',['tasks'=>Task::all()]);
-});
-Route::get('/users', function () {
-    return view('users');
-});
-Route::get('/index', function () {
-//    $task1 =Task::create([
-//        'title'=>'tasca1',
-//        'description'=>'tasca1',
-//        'completed'=>0
-//    ]);
-//    $tasks = [
-//        $task1
-//    ];
-    return view('tasks',['tasks'=>Task::all()]);
-});
+Route::get('/contact', [PagesController::class,'contact']);
+Route::get('/about', [PagesController::class,'about']);
+Route::get('/tasks', [TaskController::class,'index']);
+Route::get('/users', [UserController::class,'index']);
+Route::get('/index', [TaskController::class,'index']);
